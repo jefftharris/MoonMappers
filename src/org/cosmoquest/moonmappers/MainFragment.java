471e0c43b83f9af69382b911480e96737841c254
Jeff Harris <jefftharris@gmail.com>
2012-02-05 17:11:39 -0500

    Add main fragment to show image and four buttons for best match
    Signed-off-by: Jeff Harris <jefftharris@gmail.com>

---
+++ src/org/cosmoquest/moonmappers/MainFragment.java
@@ -0,0 +1,24 @@
+package org.cosmoquest.moonmappers;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment
+{
+
+    /* (non-Javadoc)
+     * @see android.support.v4.app.Fragment#onCreateView(android.view.LayoutInflater, android.view.ViewGroup, android.os.Bundle)
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+
+}
